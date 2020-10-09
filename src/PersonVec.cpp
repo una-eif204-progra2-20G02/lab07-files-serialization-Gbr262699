ffd9b60541009c2c17a175a47d005578f8c45b58
Gaston Berdasco <gaston262699@gmail.com>
2020-10-09 09:34:41 -0600

    personVec

---
+++ src/PersonVec.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Gaston on 9/10/2020.
+//
+
+#include "PersonVec.h"
---
+++ src/PersonVec.h
@@ -0,0 +1,34 @@
+//
+// Created by Gaston on 9/10/2020.
+//
+
+#ifndef MY_PROJECT_NAME_PERSONVEC_H
+#define MY_PROJECT_NAME_PERSONVEC_H
+#include "Person.h"
+#include <vector>
+
+class PersonVec {
+
+private:
+    vector<Person> Per;
+public:
+    void addPerson(Person);
+
+    int size();
+
+    string toString();
+
+    ~PersonVec();
+
+    Person getPerson(int position);
+
+    PersonVec(vector<struct Person> vector);
+
+    PersonVec();
+};
+
+
+
+
+
+#endif //MY_PROJECT_NAME_PERSONVEC_H
